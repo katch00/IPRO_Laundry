0cce34c3e45c9bd94542c2b96c21322b803e0e2e
katch00 <nstroupe0215@gmail.com>
2021-03-03 02:30:00 +0000

    backend start, datastore

--- laundry-app/src/main/java/myapp/DemoServlet.java
+++ laundry-app/src/main/java/myapp/DemoServlet.java
@@ -1,31 +1,58 @@
-/*
- * Copyright 2016 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package myapp;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.String;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.*;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+//@WebServlet("/machines")
 public class DemoServlet extends HttpServlet {
-  @Override
-  public void doGet(HttpServletRequest req, HttpServletResponse resp)
-      throws IOException {
-    resp.setContentType("text/plain");
-    resp.getWriter().println("{ \"name\": \"World\" }");
-  }
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/plain");
+        resp.getWriter().println("{ \"name\": \"World\" }");
+    }
+
+    @Override
+        public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String machineName = getParameter(request, "machine-name-input", "");
+        String location = getParameter(request, "machine-location-input", "MSV");
+        String machineType = getParameter(request, "machine-type-input", "washer");
+        String status = getParameter(request, "machine-status-input", "online");
+        long timestamp = System.currentTimeMillis();
+    
+
+        Entity mchnEntity = new Entity("machine");
+        mchnEntity.setProperty("name", machineName);
+        mchnEntity.setProperty("location", location);
+        mchnEntity.setProperty("type", machineType);
+        mchnEntity.setProperty("status", status);
+        mchnEntity.setProperty("timestamp", timestamp);
+
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        datastore.put(mchnEntity);
+
+        response.sendRedirect("/index.html");
+    }
+  
+    private String getParameter(HttpServletRequest request, String name, String defaultValue) {
+        String value = request.getParameter(name);
+        if (value == null) {
+            return defaultValue;
+        }
+        return value;
+    }
 }
