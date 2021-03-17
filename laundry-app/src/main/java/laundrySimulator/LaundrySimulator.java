268ed8d1507a13d2d6f1e973962a0ff62d1bd73a
asude <aozturk2@hawk.iit.edu>
2021-03-17 03:54:06 +0300

    simulator

---
+++ laundry-app/src/main/java/laundrySimulator/LaundrySimulator.java
@@ -0,0 +1,48 @@
+
+import java.util.Scanner;
+
+public class LaundrySimulator{
+	public static void main(String[] args){
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Creating a laundry simulation...");
+		Simulator s = new Simulator();
+		s.start();
+		System.out.println("Simulation has started");
+
+		while(true){
+			System.out.print("Action: ");
+			String action = input.nextLine();
+			String location;
+
+			switch (action) {
+				case "all":
+					System.out.println(s.allMachines());
+					break;
+				case "all location":
+					System.out.print("Enter location: ");
+					location = input.nextLine();
+					System.out.println(s.allMachinesByLoc(location));
+					break;
+				case "num all":
+					System.out.println(s.numAvailable());
+					break;
+					case "num location":
+						System.out.print("Enter location: ");
+						location = input.nextLine();
+						System.out.println(s.numAvailableByLoc(location));
+						break;
+				case "end":
+					System.out.println("Ending the simulation.");
+					System.exit(0);
+					break;
+				default:
+					System.out.println("Wrong option! Try again");
+					break;
+			}
+		}
+
+
+  }
+}
