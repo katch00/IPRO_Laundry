1070cdb19afa763d44c4b18b21fdd3056f5dd660
asude <aozturk2@hawk.iit.edu>
2021-03-23 23:47:24 +0300

    Upload the correct Machine file

--- laundry-app/src/main/java/laundrySimulator/Machine.java
+++ laundry-app/src/main/java/laundrySimulator/Machine.java
@@ -1,205 +1,86 @@
-import java.util.ArrayList;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
-public class Simulator{
+public class Machine {
 
-  private ArrayList<Machine> washingMachines;
-  private ArrayList<Machine> dryers;
-  private ArrayList<String> locations;
-  private final int machineNum = 5;
+  private int machineID;
+  private String machineType;
+  private String location;
 
-  public Simulator() {
-    washingMachines = new ArrayList<>();
-    dryers = new ArrayList<>();
-    locations = new ArrayList<>();
-    setLocations();
-    setMachines();
+  private boolean isFunctioning;
+  private boolean inUse;
+  public static int objectNum = 1;
+
+  private LocalDateTime start;
+  private LocalDateTime end;
+  private long elapsedTime;
+
+  public Machine() {
+    setMachineID(objectNum);
+    setMachineType("unknown");
+    setLocation("unknown");
+    setIsFunctioning(false);
+    setInUse(false);
+    objectNum++;
+  }
+  public Machine( String type, String loc) {
+    setMachineID(objectNum);
+    setMachineType(type);
+    setLocation(loc);
+    setIsFunctioning(true);
+    setInUse(false);
+    objectNum++;
+  }
+
+  public int getMachineID() { return machineID; }
+  public String getMachineType() { return machineType; }
+  public String getLocation() { return location; }
+  public boolean getIsFunctioning() { return isFunctioning; }
+  public boolean getInUse() { return inUse; }
+  public LocalDateTime getStart(){ return start; }
+  public LocalDateTime getEnd(){ return end; }
+  public long getElapsedTime() { return elapsedTime; }
+
+  public void setMachineID(int id) { machineID = id; }
+  public void setMachineType (String type) { machineType = type; }
+  public void setLocation (String loc) { location = loc; }
+  public void setIsFunctioning(boolean b) { isFunctioning = b; }
+  public void setInUse(boolean b) { inUse = b;}
+
+  public void setStart() {
+    start = LocalDateTime.now();
+  }
+  public void setEnd(int minute) {
+    end = start.plusMinutes(minute);
+  }
+  public void setElapsedTime(){
+    LocalDateTime time = LocalDateTime.now();
+    elapsedTime=  Duration.between(time,end).toMillis();
+    if( elapsedTime < 0) {
+      elapsedTime = 0;
+      inUse = false;
+    }
 
   }
 
-  public void start() {
-    breakdownMachines();
-    scheduleMachines();
+  public String getReportAll() {
+    return  "ID: " + machineID + " Type: " + machineType + " Location: " + location
+            + " is Functioning: " + isFunctioning + " in Use: " + inUse
+            + " Time Remaining: " + (elapsedTime/60000) + " min";
   }
 
-  public void setLocations() {
-    locations.add("MSV Lewis");
-    locations.add("MSV Grad");
-    locations.add("MSV East");
-    locations.add("MSV South");
-    locations.add("MSV North");
-    locations.add("MSV Fowler");
+  public String getReportLoc() {
+    return  "ID: " + machineID + " Type: " + machineType
+            + " is Functioning: " + isFunctioning + " in Use: " + inUse
+            + " Time Remaining: " + (elapsedTime/60000) + " min";
   }
 
-  public void setMachines() { //adds <machineNum> number of washing machines&dryers for each location
-    for (int i=0; i < locations.size(); i++) {
-      for(int j=0; j< machineNum; j++){
-        washingMachines.add(new Machine("washing", locations.get(i)));
-        dryers.add(new Machine("dryer", locations.get(i)));
-      }
-    }
+  public String toString() {
+    return "ID: " + machineID + " Type: " + machineType + " Location: " + location
+          + " is Functioning: " + isFunctioning + " in Use: "
+          + inUse + " Start: " + start + " End: " + end;
   }
 
-  public void scheduleMachines() { //schedule the machines randomly
-    String[] timePeriod = {"rush", "non-rush"};
-    String randomTime = timePeriod[(int)(Math.random() * 2)];
 
-    if(randomTime.equals("rush")) {
-      int washingNum = (int)(washingMachines.size()*0.5);
-      int dryerNum = (int)(dryers.size()*0.5);
-      scheduleRandom(washingNum, dryerNum);
-    }
-    else { //non-rush
-      int washingNum = (int)(washingMachines.size()*0.3);
-      int dryerNum = (int)(dryers.size()*0.3);
-      scheduleRandom(washingNum, dryerNum);
-    }
-  }
-
-  public void scheduleRandom(int washingNum, int dryerNum) {
-    int randomMachine;
-    for (int i=0; i < washingNum; i++) {
-      randomMachine = (int)(Math.random() * (washingMachines.size()-1))+1;
-      Machine m = washingMachines.get(randomMachine);
-      while(m.getInUse() || !m.getIsFunctioning()) {
-        randomMachine = (int)(Math.random() * (washingMachines.size()-1))+1;
-        m = washingMachines.get(randomMachine);
-      }
-      //start the washing machine
-      int randomTime = (int)(Math.random() * 40);
-      m.setInUse(true);
-      m.setStart();
-      m.setEnd(randomTime);
-    }
-    for (int i=0; i < dryerNum; i++) {
-      randomMachine = (int)(Math.random() * (dryers.size()-1))+1;
-      Machine m = dryers.get(randomMachine);
-      while(m.getInUse() || !m.getIsFunctioning()) {
-        randomMachine = (int)(Math.random() * (dryers.size()-1))+1;
-        m = dryers.get(randomMachine);
-      }
-      //start the dryer
-      int randomTime = (int)(Math.random() * 40) + 5;
-      m.setStart();
-      m.setEnd(randomTime);
-    }
-  }
-
-  public void breakdownMachines(){
-    int washingNum = (int)(washingMachines.size()*0.1);
-    int dryerNum = (int)(dryers.size()*0.1);
-    int randomMachine;
-
-    for (int i=0; i < washingNum; i++) {
-      randomMachine = (int)(Math.random() * (dryers.size()-1))+1;
-      Machine m = washingMachines.get(randomMachine);
-      while(!m.getInUse() && !m.getIsFunctioning()) {
-        randomMachine = (int)(Math.random() * (dryers.size()-1))+1;
-        m = washingMachines.get(randomMachine);
-      }
-      m.setIsFunctioning(false);
-    }
-
-    for (int i=0; i < dryerNum; i++) {
-      randomMachine = (int)(Math.random() * (dryers.size()-1))+1;
-      Machine m = dryers.get(randomMachine);
-      while(!m.getInUse() && !m.getIsFunctioning()) {
-        randomMachine = (int)(Math.random() * (dryers.size()-1))+1;
-        m = dryers.get(randomMachine);
-      }
-      m.setIsFunctioning(false);
-    }
-}
-
-  public String allMachines() {
-    String report ="All washing machines and dryers on campus\n";
-    calculateRemaining();
-    for(int i=0; i < washingMachines.size(); i++){
-      report += "\n" + washingMachines.get(i).getReportAll();
-    }
-    for(int i=0; i<dryers.size(); i++){
-      report += "\n" + dryers.get(i).getReportAll();
-    }
-    return report;
-
-  }
-
-  public String allMachinesByLoc(String location) {
-    String report ="All washing machines and dryers in: " + location + "\n";
-    calculateRemaining();
-    for(int i=0; i < washingMachines.size(); i++){
-      if (washingMachines.get(i).getLocation().equals(location))
-        report += "\n" + washingMachines.get(i).getReportLoc();
-    }
-    for(int i=0; i<dryers.size(); i++){
-      if (dryers.get(i).getLocation().equals(location))
-        report += "\n" + dryers.get(i).getReportLoc();
-    }
-    return report;
-  }
-
-  public String numAvailable() {
-    String report = "Number of available machines in all locations: \n";
-    Machine mc;
-    calculateRemaining();
-    for(int i=0; i < locations.size(); i++){
-      String loc = locations.get(i);
-      int dCount = 0, lCount = 0;
-
-      for(int j=0; j < washingMachines.size(); j++){
-        mc = washingMachines.get(j);
-        if (mc.getLocation().equals(loc)) {
-           if(!mc.getInUse() && mc.getIsFunctioning())
-              lCount++;
-        }
-      }
-      for(int j=0; j < dryers.size(); j++){
-        mc = dryers.get(j);
-        if (mc.getLocation().equals(loc)) {
-           if(!mc.getInUse() && mc.getIsFunctioning())
-              dCount++;
-        }
-      }
-      report += loc + " Washing Machines: " + lCount + " Dryers: " + dCount + "\n";
-    }
-    return report;
-  }
-
-  public String numAvailableByLoc(String location){
-    String report = "Number of available machines in: " + location +"\n";
-    Machine mc;
-    int dCount = 0, lCount = 0;
-    calculateRemaining();
-
-    for(int i=0; i < washingMachines.size(); i++){
-      mc = washingMachines.get(i);
-      if (mc.getLocation().equals(location)) {
-        if(!mc.getInUse() && mc.getIsFunctioning())
-          lCount++;
-        }
-    }
-
-    for(int i=0; i < dryers.size(); i++){
-      mc = dryers.get(i);
-      if (mc.getLocation().equals(location)) {
-          if(!mc.getInUse() && mc.getIsFunctioning())
-            dCount++;
-      }
-    }
-     return report += "Washing Machines: " + lCount + " Dryers: " + dCount + "\n";
-  }
-
-  public void calculateRemaining() {
-
-    for(int i=0; i < washingMachines.size(); i++){
-      if(washingMachines.get(i).getInUse()) {
-        washingMachines.get(i).setElapsedTime();
-      }
-    }
-    for(int i=0; i<dryers.size(); i++){
-      if(dryers.get(i).getInUse()) {
-        dryers.get(i).setElapsedTime();
-      }
-    }
-
-  }
 }
