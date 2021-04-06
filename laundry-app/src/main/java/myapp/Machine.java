d617876fcaa4e0ab128b6a8bcb7706684ac60a46
katch00 <64349523+katch00@users.noreply.github.com>
2021-04-06 18:13:33 -0500

    Connecting simulator (#72)
    * connecting the simulator
    
    * connecting the simulator
    
    * clean up code, minor fixes
    
    Co-authored-by: asudeozturk <aozturk2@hawk.iit.edu>

--- laundry-app/src/main/java/myapp/Machine.java
+++ laundry-app/src/main/java/myapp/Machine.java
@@ -1,21 +1,93 @@
 package myapp;
 
-public final class Machine {
-    private final long id;
-    private final String name;
-    private final long timestamp;
-    private final String location;
-    private final String status;
-    private final String type;
-    private final String testing;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
-    public Machine(long id, String name, long timestamp, String location, String status, String type, String testing) {
-        this.id = id;
-        this.name = name;
-        this.timestamp = timestamp;
-        this.location = location;
-        this.status = status;
-        this.type = type;
-        this.testing = testing;
+
+public class Machine implements Comparable{
+
+  private long id;
+  private String name;
+  private String type;
+  private String location;
+  private String status;
+
+  private LocalDateTime end;
+  private long timeRemaining;
+  private String testing;
+
+  public static long objectNum = 1;
+
+  
+  //creating new machine, called by Simulator
+  public Machine( String type, String loc) {
+    setID(objectNum);
+    setName(String.valueOf(id));
+    setType(type);
+    setLocation(loc);
+    setStatus("idle");
+    setEnd(LocalDateTime.of(0,1,1,0,0,0));
+    setTimeRemaining(0);
+    setTesting("");
+    objectNum++;
+  }
+
+  //copying a machine for records, called by Machines 
+  public Machine(long i, String n, String typ, String loc, String stat,  LocalDateTime endTime,  String test ){
+    setID(i);
+    setName(n);
+    setType(typ);
+    setLocation(loc);
+    setStatus(stat);
+    setEnd(endTime);
+    setTimeRemaining();
+    setTesting(test);
+  }
+
+  public long  getID() { return id; }
+  public String getName() { return name;}
+  public String getType() { return type; }
+  public String getLocation() { return location; }
+  public String getStatus() { return status;}
+  public LocalDateTime getEnd(){ return end; }
+  public long getTimeRemaining() { return timeRemaining; }
+  public String getTesting() { return testing;}
+
+  public void setID(long i) { id  = i; }
+  public void setName(String n) { name = n;}
+  public void setType (String t) { type = t; }
+  public void setLocation (String l) { location = l; }
+  public void setStatus (String s) { status = s; }
+  public void setTesting(String t) { testing = t;}
+  public void setTimeRemaining(long t) { timeRemaining = t; }
+  public void setEnd(LocalDateTime t) { end = t; } //called from Machines
+  public void setEnd(int minute) { //called from Simulator
+    LocalDateTime start = LocalDateTime.now();
+    end = start.plusMinutes(minute);
+  }
+ 
+  public void setTimeRemaining(){
+    LocalDateTime time = LocalDateTime.now();
+    timeRemaining=  Duration.between(time,end).toMillis()/60000;
+    if( timeRemaining < 1 && status.equals("busy")) {
+      timeRemaining = 0;
+      setStatus("idle");
+      setEnd(LocalDateTime.of(0,1,1,0,0,0));
     }
+
+  }
+
+  public String toString() {
+    return "ID: " + id + " Name: " + name + " Type: " + type + " Location: " + location
+          + " Status: " + status + " End: " + end  + " Time Remaining: " + timeRemaining ;
+  }
+
+  @Override //for sorting the list
+    public int compareTo(Object o) {
+        long idCompare = ((Machine)o).getID();
+        return (int)(this.id  - idCompare);
+    }
+
 }
+
---
+++ laundry-app/src/main/java/myapp/Simulator.java
@@ -0,0 +1,147 @@
+package myapp;
+import java.util.ArrayList;
+
+
+public class Simulator{
+
+  private ArrayList<Machine> washers;
+  private ArrayList<Machine> dryers;
+  private ArrayList<String> locations;
+  private final int machineNum = 5;
+
+ public Simulator() {
+    washers = new ArrayList<>();
+    dryers = new ArrayList<>();
+    locations = new ArrayList<>();
+    setLocations();
+    setMachines();
+    calculateRemaining();
+
+  }
+
+  public void start() {
+    breakdownMachines();
+    scheduleMachines();
+  }
+
+  public void setLocations() {
+    locations.add("Guns");
+    locations.add("MSV");
+    locations.add("SSV");
+    locations.add("Carmen");
+
+  }
+
+  //adds <machineNum> amount of washers and <machineNum> amount if dryer to each location
+  public void setMachines() {
+    for (int i=0; i < locations.size(); i++) {
+      for(int j=0; j< machineNum; j++){
+        washers.add(new Machine("washer", locations.get(i)));
+        dryers.add(new Machine("dryer", locations.get(i)));
+      }
+    }
+  }
+
+  //determine random nubmer num of machines to start
+  public void scheduleMachines() {
+    String[] timePeriod = {"rush", "non-rush"};
+    String randomTime = timePeriod[(int)(Math.random() * 2)];
+    int washerNum, dryerNum;
+
+    if(randomTime.equals("rush")) {
+      washerNum = (int)(washers.size()*0.5);
+      dryerNum = (int)(dryers.size()*0.5);
+    }
+    else { //non-rush
+      washerNum = (int)(washers.size()*0.3);
+      dryerNum = (int)(dryers.size()*0.3);
+    }
+      
+    scheduleRandom(washerNum, dryerNum);
+ 
+  }
+
+  //choose machines randomly by their id 
+  public void scheduleRandom(int washingNum, int dryerNum) {
+    int randomMachine;
+    for (int i=0; i < washingNum; i++) {
+      randomMachine = (int)(Math.random() * (washers.size()-1))+1;
+      Machine m = washers.get(randomMachine);
+      while(m.getStatus().equals("busy")) {
+        randomMachine = (int)(Math.random() * (washers.size()-1))+1;
+        m = washers.get(randomMachine);
+      }
+      //start the washing machine
+      int randomTime = (int)(Math.random() * 40);
+      m.setStatus("busy");
+      m.setEnd(randomTime);
+    }
+
+    for (int i=0; i < dryerNum; i++) {
+      randomMachine = (int)(Math.random() * (dryers.size()-1))+1;
+      Machine m = dryers.get(randomMachine);
+      while(m.getStatus().equals("busy")) {
+        randomMachine = (int)(Math.random() * (dryers.size()-1))+1;
+        m = dryers.get(randomMachine);
+      }
+      //start the dryer
+      int randomTime = (int)(Math.random() * 40) + 5;
+      m.setStatus("busy");
+      m.setEnd(randomTime);
+    }
+  }
+
+  //choose random machines by id and set status=offline
+  public void breakdownMachines(){
+    int washerNum = (int)(washers.size()*0.1);
+    int dryerNum = (int)(dryers.size()*0.1);
+    int randomMachine;
+
+    for (int i=0; i < washerNum; i++) {
+      randomMachine = (int)(Math.random() * (washers.size()-1))+1;
+      Machine m = washers.get(randomMachine);
+      while(m.getStatus().equals("offline")) {
+        randomMachine = (int)(Math.random() * (washers.size()-1))+1;
+        m = washers.get(randomMachine);
+      }
+      m.setStatus("offline");
+    }
+
+    for (int i=0; i < dryerNum; i++) {
+      randomMachine = (int)(Math.random() * (dryers.size()-1))+1;
+      Machine m = dryers.get(randomMachine);
+      while(m.getStatus().equals("offline")) {
+        randomMachine = (int)(Math.random() * (dryers.size()-1))+1;
+        m = dryers.get(randomMachine);
+      }
+      m.setStatus("offline");
+    }
+  }
+
+  //calculates time remaining for each machine
+  public void calculateRemaining() {
+    for(int i=0; i < washers.size(); i++){
+      if(washers.get(i).getStatus().equals("busy")) {
+        washers.get(i).setTimeRemaining();
+      }
+    }
+    for(int i=0; i<dryers.size(); i++){
+      if(dryers.get(i).getStatus().equals("busy")) {
+        dryers.get(i).setTimeRemaining();
+      }
+    }
+
+  }
+
+  //returns all machines 
+  public ArrayList<Machine> getMachines() {
+    calculateRemaining();
+    ArrayList<Machine> allMachine = new ArrayList<>();
+    allMachine.addAll(washers);
+    allMachine.addAll(dryers);
+    return allMachine;
+
+  }
+
+
+}
